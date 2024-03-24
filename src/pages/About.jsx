52d3fc9e28ca90a5bc04a4bae241b1d9daf7aa5c
Ahmidou EL mehdi <ahmidouem@gmail.com>
2024-03-24 18:27:12 +0000

    Added a profile image and introduction text to the About page for a more personalized touch.    - Updated the experience and education sections with relevant details.    - Added a skills section with a list of skills.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,35 +4,149 @@ import styled from "styled-components";
 import ComponentStatistics from "../features/about/ComponentStatistics";
 import AccordionWrapper from "../features/about/AccordionWrapper";
 import Accordion from "../features/about/Accordion";
+import profileImage from "../assets/img/profile.jpg";
+import Footer from "../features/footer/Footer";
 
 const panels = [
   {
-    label: "Seriously, Don't Use Icon Fonts",
-    content:
-      'Icons are everywhere. These "little miracle workers" (as John Hicks described them) help us reinforce meaning in the interfaces we design and build. Their popularity in web design has never been greater; the conciseness and versatility of pictograms in particular make them a lovely fit for displays large and small. But icons on the web have had their fair share of challenges.',
+    label: <h2>ABOUT ME</h2>,
+    content: (
+      <p>
+        El Mehdi Ahmidou transitioned from a role as a Math Teacher with the
+        Ministry of National Education in Rabat, Morocco, where he delivered
+        tailored instruction to cater to diverse learning needs, fostering
+        personalized learning experiences and collaborating with peers to devise
+        interdisciplinary projects. Subsequently, he pursued a career shift
+        towards technology, enrolling in the Remote Full Stack Web Development
+        Program at MICROVERSE, where he honed his skills in algorithms, data
+        structures, and frontend development while managing projects using
+        JavaScript, React, and Redux.
+      </p>
+    ),
   },
   {
-    label: "Screen Readers Actually Read That Stuff",
-    content:
-      'When your icon font fails, the browser treats it like any other font and replaces it with a fallback. Best-case scenario, you\'ve chosen your fallback characters carefully and something weird-looking but communicative still loads. Worse-case scenario (and far more often), the user sees something completely incongruous, usually the dreaded "missing character" glyph. When your icon font fails, the browser treats it like any other font and replaces it with a fallback. Best-case scenario, you\'ve chosen your fallback characters carefully and something weird-looking but communicative still loads. Worse-case scenario (and far more often), the user sees something completely incongruous, usually the dreaded "missing character" glyph When your icon font fails, the browser treats it like any other font and replaces it with a fallback. Best-case scenario, you\'ve chosen your fallback characters carefully and something weird-looking but communicative still loads. Worse-case scenario (and far more often), the user sees something completely incongruous, usually the dreaded "missing character" glyph When your icon font fails, the browser treats it like any other font and replaces it with a fallback. Best-case scenario, you\'ve chosen your fallback characters carefully and something weird-looking but communicative still loads. Worse-case scenario (and far more often), the user sees something completely incongruous, usually the dreaded "missing character" glyph When your icon font fails, the browser treats it like any other font and replaces it with a fallback. Best-case scenario, you\'ve chosen your fallback characters carefully and something weird-looking but communicative still loads. Worse-case scenario (and far more often), the user sees something completely incongruous, usually the dreaded "missing character" glyph',
+    label: <h2>EXPERIENCE</h2>,
+    content: (
+      <ul>
+        <li>
+          <h3>Front-End Developer - Remote</h3>
+          <p>Jan 2022 - Present</p>
+          <p>
+            Developed capstone projects utilizing various technologies including
+            HTML, CSS, JavaScript, and React.
+          </p>
+        </li>
+        <li>
+          <h3>Math Teacher - Ministry of National Education</h3>
+          <p>Jan 2018 - Jan 2022</p>
+          <p>
+            Delivered tailored instruction, fostered personalized learning
+            experiences, and collaborated with peers on interdisciplinary
+            projects.
+          </p>
+        </li>
+      </ul>
+    ),
   },
   {
-    label: "They Fail Poorly and Often",
-    content:
-      'When your icon font fails, the browser treats it like any other font and replaces it with a fallback. Best-case scenario, you\'ve chosen your fallback characters carefully and something weird-looking but communicative still loads. Worse-case scenario (and far more often), the user sees something completely incongruous, usually the dreaded "missing character" glyph. When your icon font fails, the browser treats it like any other font and replaces it with a fallback. Best-case scenario, you\'ve chosen your fallback characters carefully and something weird-looking but communicative still loads. Worse-case scenario (and far more often), the user sees something completely incongruous, usually the dreaded "missing character" glyph When your icon font fails, the browser treats it like any other font and replaces it with a fallback. Best-case scenario, you\'ve chosen your fallback characters carefully and something weird-looking but communicative still loads. Worse-case scenario (and far more often), the user sees something completely incongruous, usually the dreaded "missing character" glyph When your icon font fails, the browser treats it like any other font and replaces it with a fallback. Best-case scenario, you\'ve chosen your fallback characters carefully and something weird-looking but communicative still loads. Worse-case scenario (and far more often), the user sees something completely incongruous, usually the dreaded "missing character" glyph When your icon font fails, the browser treats it like any other font and replaces it with a fallback. Best-case scenario, you\'ve chosen your fallback characters carefully and something weird-looking but communicative still loads. Worse-case scenario (and far more often), the user sees something completely incongruous, usually the dreaded "missing character" glyph',
+    label: <h2>EDUCATION</h2>,
+    content: (
+      <ul>
+        <li>
+          <h3>Remote Full Stack Web Development Program - MICROVERSE</h3>
+          <p>May 2023 - Present</p>
+          <p>
+            Mastered algorithms, data structures, and frontend development.
+            Managed projects using JavaScript, React, and Redux.
+          </p>
+        </li>
+        <li>
+          <h3>
+            Master's degree in Physics Informatics - Faculty of Sciences Rabat
+          </h3>
+          <p>Sep 2015 - Sep 2017</p>
+          <p>Gained expertise in mathematics, physics, and programming.</p>
+        </li>
+      </ul>
+    ),
+  },
+  {
+    label: <h2>SKILLS</h2>,
+    content: (
+      <ul>
+        <li>Front-End: JavaScript, React, Redux, HTML5, CSS3</li>
+        <li>
+          Tools & Methods: Git, GitHub, Mobile/Responsive Development, Chrome
+          Dev Tools
+        </li>
+        <li>Professional: Remote Pair-Programming, Teamwork</li>
+      </ul>
+    ),
   },
 ];
 
-const Container = styled.div``;
-function About() {
+const AboutContainer = styled.div`
+  max-width: 120rem;
+  margin: 0 auto;
+  padding: 12rem 0;
+`;
+
+const IntroContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 5rem;
+  gap: 6rem;
+`;
+
+const IntroImage = styled.div`
+  margin-right: 2rem;
+`;
+
+const Image = styled.img`
+  border-radius: 50%;
+  max-width: 30rem;
+  height: auto;
+`;
+
+const IntroText = styled.div`
+  h1 {
+    font-size: 3.4rem;
+    margin-bottom: 2rem;
+  }
+
+  p {
+    font-weight: 400;
+    line-height: 1.5;
+    font-size: 2.2rem;
+  }
+`;
+
+function About(color) {
   return (
-    <Container>
-      <h1>I love being a developer !</h1>
-      <ComponentStatistics />
+    <AboutContainer>
+      <IntroContainer>
+        <IntroImage>
+          <Image src={profileImage} alt="EL MEHDI AHMIDOU" />
+        </IntroImage>
+        <IntroText>
+          <h1>Hello, I'm EL MEHDI AHMIDOU 🙂</h1>
+          <p>
+            {" "}
+            I’m Front End Developer, located in Rabat, Morocco. With an
+            experience of 4 years, I have built web applications for clients
+            from different countries and industries. My main objective is to
+            create interfaces that are engaging, accessible, and high-performing
+            for humans.
+          </p>
+          <ComponentStatistics />
+        </IntroText>
+      </IntroContainer>
       <AccordionWrapper>
         <Accordion panels={panels} />
       </AccordionWrapper>
-    </Container>
+      <Footer style={{ color: "var(--color-primary-3)" }} />
+    </AboutContainer>
   );
 }
 
