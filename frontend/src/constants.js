ca218ceabee8b5ab686f9e90b09320760ca95e40
Ahmidou EL mehdi <ahmidouem@gmail.com>
2026-02-17 14:17:57 +0000

    feat: introduce API endpoint constants for base URL, projects, users, and upload.

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000" : "";
+export const BASE_URL = import.meta.env.MODE === "development" ? "https://www.elmehdi.cv" : "";
 export const PROJECTS_URL = "/api/projects";
 export const USERS_URL = "/api/users";
 export const UPLOAD_URL = "/api/upload/";
